ea0c7874dcdedbea3cb2c32f375c3b60b0c1caf3
luchn <luc.hn@teko.vn>
2021-01-21 17:50:39 +0700

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -12,9 +12,6 @@
       backgroundRepeat: 'no-repeat',
       backgroundPosition: 'center bottom'
     }">Canvas not supported.</canvas>
-    <div>
-      <img src="https://png.pngtree.com/thumb_back/fh260/background/20190828/pngtree-dark-vector-abstract-background-image_302715.jpg" />
-    </div>
     <div class="controller">
     </div>
   </div>
