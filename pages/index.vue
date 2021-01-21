2947bfaa9834b2cb9c03d4de354337898f995073
Luc Hoang <luc.hn@teko.vn>
2021-01-21 23:25:11 +0700

    teko page

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,19 @@
 <template>
-  <!-- <div class="container">
-   <div>
-     <img class="image" :src="logo" />
-     <div class="text">Something new is going to deploy</div>
-   </div>
-  </div> -->
-  <div>
-    <canvas id="canvas" :style="{
-      backgroundImage: `url(${logo})`,
-      backgroundSize: 'contain',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center bottom'
-    }">Canvas not supported.</canvas>
-    <div class="controller">
-    </div>
+  <div class="fireworks">
+    <vs-button to="/teko" class="btn-teko" warn gradient
+      >Tìm hiểu Teko</vs-button
+    >
+    <canvas
+      id="canvas"
+      :style="{
+        backgroundImage: `url(${logo})`,
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center bottom',
+      }"
+      >Canvas not supported.</canvas
+    >
+    <div class="controller"></div>
   </div>
 </template>
 
@@ -26,25 +26,29 @@ export default {
     VuesaxLogo,
   },
   data: function () {
-      return {
-          logo: logo
-      }
+    return {
+      logo: logo,
+    }
   },
   mounted() {
-    var background = new Image();
-    background.src = logo;
-    var count = document.getElementById('count');
-    var countBall = 6;
+    var background = new Image()
+    background.src = logo
+    var count = document.getElementById('count')
+    var countBall = 6
     if (count && count.value) {
       countBall = count.value
     }
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-      countBall = 2;
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      countBall = 2
     }
-    var canvas = document.getElementById('canvas');
+    var canvas = document.getElementById('canvas')
 
     if (!canvas || !canvas.getContext) {
-      return false;
+      return false
     }
 
     /********************
@@ -52,17 +56,17 @@ export default {
     ********************/
 
     function rand(min, max) {
-      return Math.floor(Math.random() * (max - min + 1) + min);
+      return Math.floor(Math.random() * (max - min + 1) + min)
     }
 
     /********************
       Var
     ********************/
 
-    // canvas 
-    var ctx = canvas.getContext('2d');
-    var X = canvas.width = window.innerWidth;
-    var Y = canvas.height = window.innerHeight;
+    // canvas
+    var ctx = canvas.getContext('2d')
+    var X = (canvas.width = window.innerWidth)
+    var Y = (canvas.height = window.innerHeight)
 
     /********************
       Animation
@@ -73,184 +77,191 @@ export default {
       window.mozRequestAnimationFrame ||
       window.webkitRequestAnimationFrame ||
       window.msRequestAnimationFrame ||
-      function(cb) {
-        setTimeout(cb, 17);
-      };
+      function (cb) {
+        setTimeout(cb, 17)
+      }
 
     /********************
       Ball
     ********************/
-    
+
     // var
-    var ballNum = countBall;
-    var balls = [];
-     
+    var ballNum = countBall
+    var balls = []
+
     function Ball(ctx, x, y) {
-      this.ctx = ctx;
-      this.init(x, y);
+      this.ctx = ctx
+      this.init(x, y)
     }
 
-    Ball.prototype.init = function(x, y) {
-      this.x = x;
-      this.y = y;
-      this.r = rand(1, 3);
+    Ball.prototype.init = function (x, y) {
+      this.x = x
+      this.y = y
+      this.r = rand(1, 3)
       this.v = {
-        y: rand(1, 5) * 1
-      };
-      this.bloomY = rand(0, Y / 2);
-    };
-
-    Ball.prototype.draw = function() {
-      var ctx = this.ctx;
-      ctx.beginPath();
-      ctx.fillStyle = 'rgb(153, 153, 153)';
-      ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
-      ctx.fill();
-    };
-
-    Ball.prototype.wrapPosition = function() {
-      if (this.y < this.bloomY) {
-        bloomFire(ctx, this.x, this.y, getColor(), rand(0.8, 3));
-        this.y = Y;
-        this.x = rand(0, X);
+        y: rand(1, 5) * 1,
       }
-    };
+      this.bloomY = rand(0, Y / 2)
+    }
 
-    Ball.prototype.updatePosition = function() {
-      this.y -= this.v.y;
-    };
+    Ball.prototype.draw = function () {
+      var ctx = this.ctx
+      ctx.beginPath()
+      ctx.fillStyle = 'rgb(153, 153, 153)'
+      ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, false)
+      ctx.fill()
+    }
 
-    Ball.prototype.resize = function() {
-      this.x = rand(0, X);
-    };
+    Ball.prototype.wrapPosition = function () {
+      if (this.y < this.bloomY) {
+        bloomFire(ctx, this.x, this.y, getColor(), rand(0.8, 3))
+        this.y = Y
+        this.x = rand(0, X)
+      }
+    }
 
-    Ball.prototype.render = function() {
-      this.updatePosition();
-      this.wrapPosition();
-      this.draw();
-    };
+    Ball.prototype.updatePosition = function () {
+      this.y -= this.v.y
+    }
+
+    Ball.prototype.resize = function () {
+      this.x = rand(0, X)
+    }
+
+    Ball.prototype.render = function () {
+      this.updatePosition()
+      this.wrapPosition()
+      this.draw()
+    }
 
     for (var i = 0; i < ballNum; i++) {
-      var ball = new Ball(ctx, rand(0, X), Y);
-      balls.push(ball);
+      var ball = new Ball(ctx, rand(0, X), Y)
+      balls.push(ball)
     }
 
     /********************
       Fire
     ********************/
-    
+
     // var
-    var fires = [];
-    var fireGravity = 0.01;
+    var fires = []
+    var fireGravity = 0.01
 
     function getColor() {
-      return "#925F2A";
+      return '#925F2A'
     }
     function Fire(ctx, x, y, c, d, r) {
-      this.ctx = ctx;
-      this.init(x, y, c, d, r);
+      this.ctx = ctx
+      this.init(x, y, c, d, r)
     }
 
-    Fire.prototype.init = function(x, y, c, d, r) {
-      this.rad = r * Math.PI / 180;
-      this.x = x;
-      this.y = y;
-      this.r = 2;
-      this.c = c;
-      this.d = d;
-      this.l = rand(10, 20);
+    Fire.prototype.init = function (x, y, c, d, r) {
+      this.rad = (r * Math.PI) / 180
+      this.x = x
+      this.y = y
+      this.r = 2
+      this.c = c
+      this.d = d
+      this.l = rand(10, 20)
       this.v = {
         x: Math.cos(this.rad) * this.d,
-        y: Math.sin(this.rad) * this.d
-      };
-    };
+        y: Math.sin(this.rad) * this.d,
+      }
+    }
 
-    Fire.prototype.draw = function() {
-      var ctx = this.ctx;
-      ctx.beginPath();
-      ctx.globalCompositeOperation = 'lighter';
-      ctx.fillStyle = this.c;
-      ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
-      ctx.fill();
-    };
+    Fire.prototype.draw = function () {
+      var ctx = this.ctx
+      ctx.beginPath()
+      ctx.globalCompositeOperation = 'lighter'
+      ctx.fillStyle = this.c
+      ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI)
+      ctx.fill()
+    }
 
-    Fire.prototype.updatePosition = function() { 
-      this.v.y += fireGravity;
-      this.x += this.v.x;
-      this.y += this.v.y;
-    };
+    Fire.prototype.updatePosition = function () {
+      this.v.y += fireGravity
+      this.x += this.v.x
+      this.y += this.v.y
+    }
 
-    Fire.prototype.updateParams = function() {
-      this.l -= 0.1;
-    };
+    Fire.prototype.updateParams = function () {
+      this.l -= 0.1
+    }
 
-    Fire.prototype.deleteFire = function(i) {
-       if (this.l < 0) {
-         fires.splice(i, 1);
-       }
-    };
+    Fire.prototype.deleteFire = function (i) {
+      if (this.l < 0) {
+        fires.splice(i, 1)
+      }
+    }
 
-    Fire.prototype.resize = function() {
-      this.x = rand(0, X);
-    };
+    Fire.prototype.resize = function () {
+      this.x = rand(0, X)
+    }
 
-    Fire.prototype.render = function(i) {
-      this.updateParams();
-      this.deleteFire(i);
-      this.updatePosition();
-      this.draw();
-    };
+    Fire.prototype.render = function (i) {
+      this.updateParams()
+      this.deleteFire(i)
+      this.updatePosition()
+      this.draw()
+    }
 
     function bloomFire(ctx, x, y, c, d) {
       for (var i = 0; i < 36; i++) {
-        var fire = new Fire(ctx, x, y, c, d, i * 10);
-        fires.push(fire);
+        var fire = new Fire(ctx, x, y, c, d, i * 10)
+        fires.push(fire)
       }
     }
-         
+
     /********************
       Render
     ********************/
-    
-    function render(){
-      ctx.globalCompositeOperation = "xor";
-      ctx.globalAlpha = 0.05;
-      ctx.fillStyle = "rgb(0,0,0)";
-      ctx.fillRect(0, 0, X, Y);
-      ctx.globalAlpha = 1;
+
+    function render() {
+      ctx.globalCompositeOperation = 'xor'
+      ctx.globalAlpha = 0.05
+      ctx.fillStyle = 'rgb(0,0,0)'
+      ctx.fillRect(0, 0, X, Y)
+      ctx.globalAlpha = 1
       for (var i = 0; i < balls.length; i++) {
-        balls[i].render();
+        balls[i].render()
       }
       for (var i = 0; i < fires.length; i++) {
-        fires[i].render(i);
+        fires[i].render(i)
       }
 
-      requestAnimationFrame(render);
+      requestAnimationFrame(render)
     }
 
-    render();
+    render()
 
     /********************
       Event
     ********************/
-    
+
     function onResize() {
-      X = canvas.width = window.innerWidth;
-      Y = canvas.height = window.innerHeight;
+      X = canvas.width = window.innerWidth
+      Y = canvas.height = window.innerHeight
       for (var i = 0; i < fires.length; i++) {
-        fires[i].resize();
+        fires[i].resize()
       }
     }
 
-    window.addEventListener('resize', function() {
-      onResize();
-    });
-  }
+    window.addEventListener('resize', function () {
+      onResize()
+    })
+  },
 }
 </script>
 
 <style>
-body {
+.btn-teko {
+  position: fixed;
+  bottom: 6%;
+  left: 0;
+  right: 0;
+  margin: auto;
+}
+.fireworks {
   background: #000;
 }
 * {
---
+++ pages/teko.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="center examplex">
+    <vs-navbar text-white :color="active" center-collapsed v-model="active">
+      <template #left>
+        <img
+          class="logo"
+          src="https://tekoventures.vn/wp-content/uploads/2018/09/logo-teko.png"
+          alt=""
+        />
+      </template>
+    </vs-navbar>
+    <div class="square">
+      <div>
+        <div>
+          Website đang trong quá trình hoàn thiện. Trong thời gian chờ đợi bạn
+          có thể mua hàng tại:
+        </div>
+        <div class="flex">
+          <vs-button href="https://phongvu.vn"> Phong Vũ </vs-button>
+          <vs-button href="https://vnshop.vn" danger> Vnshop </vs-button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+  margin-top: 16px;
+}
+.examplex {
+  background: #fff;
+}
+.logo {
+  max-height: 30px;
+}
+.square {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 16px;
+}
+</style>
+
+<script>
+export default {
+  data: () => ({
+    active: 'primary',
+  }),
+}
+</script>
